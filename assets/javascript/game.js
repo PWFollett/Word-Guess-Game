8880dd2067befbf7867d8fb30e51d6aef0987254
PWFollett <pwf592110@gmail.com>
2019-05-08 18:25:01 -0400

    still trying to figure it out, getting there

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,10 @@
 // GLOBAL VARIABLES 
 // Array of Dogs
-var wordsList = ["husky", "shepard", "cockapoo", "goldenlab", "beagle", "bulldog", 
-"yorkie", "weinerdog", "mastiff", "saintbernard", "greyhound", "poodle"];
+var wordsList = ["husky", "shepard", "cockapoo", "goldenlab", "beagle", "bulldog",
+    "yorkie", "weinerdog", "mastiff", "saintbernard", "greyhound", "poodle"];
 
 // Solution 
-var Word = "_";
+var wordAnswer = "_"
 
 // Breaks the solution into individual letters to be stored in array
 var lettersInWord = [];
@@ -23,57 +23,78 @@ var winCounter = 0;
 var lossCounter = 0;
 var MaxnumGuesses = 9;
 var numGuesses = 0;
-
+//Others
 var keyPressed = false;
 var isDone = false;
+var Word = "_";
+var gamestarted = false;
 
 
 //FUNCTION
 
-document.addEventListener('keydown', function() {
-    if (keyCode == 13)
-document.onkeydown = function() {startGame()};
-function startGame(){
-    Word = wordsList[Math.floor(Math.random()* wordsList.length)];
+document.onkeydown = function (event) { 
+  //  console.log("this is our key", event.keyCode);
+    if (event.keyCode === 13 && gamestarted === false) {
+        gamestarted = true; 
+        startGame()   
+    } else {
+      // console.log("play with this letter", event.key) 
+       playGame(event.key)
+    }
+      };
+function startGame() {
+    Word = wordsList[Math.floor(Math.random() * wordsList.length)];
     console.log(Word);
     var wordReplacement = Word.replace(/./g, "-");
-    
+
     document.getElementById("word-fill").innerHTML = wordReplacement;
 }
+// PLAY GAME FUNCTION
+function playGame(userGuess) {
+    console.log("this is our user guess", userGuess);
+    console.log("current game word", Word);
+    console.log("Is letter in Word?", Word.indexOf(userGuess))
+    if (Word.indexOf(userGuess) >= 0){
+        alert(userGuess+" in this word " + Word)
+    }
 
+}
 
 // Variable Reset
 numGuesses = MaxnumGuesses
 incorrectGuesses = [];
 
 // for loop to add word?
-for( let i = 0; i < Word.length; i++) {
+for (let i = 0; i < Word.length; i++) {
     blanksAndcorrect[i] = "_";
 }
 
 // LOSER FUNCTION
-function isLoser(){
- if (numGuesses <=0) {
-    lossCounter ++ ;
-    isDone = true; 
- }
-}; 
-function checkGuess(letter){
-
-//if letter is guessed
-if (blanksAndcorrects.indexOf(letter) === -1 ) {
-    blanksAndcorrects.push(letter);
-    // if letter isnt present in answer
-    if (Word.indexOf(letter) === -1) {
-        numGuesses--;
-        if(numGuesses <= 3) {
-            document.getElementById("Guesses");
-        }
-        else {
-            for (var i = 0)
+function isLoser() {
+    if (numGuesses <= 0) {
+        lossCounter++;
+        isDone = true;
+    }
+};
+function checkGuess(letter) {
+    //if letter is guessed
+    if (blanksAndcorrects.indexOf(letter) === -1) {
+        blanksAndcorrects.push(letter);
+        // if letter isnt present in answer
+        if (Word.indexOf(letter) === -1) {
+            numGuesses--;
+            if (numGuesses <= 3) {
+                document.getElementById("Guesses");
+            }
+            else {
+                for (var i = 0; i < Word.length; i++) {
+                    if (letter === Word[i]) {
+                        wordAnswer[i] = letter;
+                    }
+                }
+            }
         }
     }
-}
 
 
-}
+};
